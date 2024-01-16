2e9bff52a1f471ae094965a0d2d731182175a680
Rodrigo Souza <rodrigo.souza@goharry.com.br>
2024-01-16 07:31:56 -0300

    Start project

---
+++ src/test/java/br/com/cronos/bitabit/threadasync/ThreadAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cronos.bitabit.threadasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
